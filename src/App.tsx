6dc175a1a79e5c19b3aeac562259cbf7115eec5b
koo <kootoomo@gmail.com>
2023-01-13 04:07:32 +0300

    add monsters with TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+// import { Component } from 'react';
+
+import { useState, useEffect, ChangeEvent } from 'react';
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
+import { getData } from './utils/data.utils';
+import './App.css';
+
+export type Monster = {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const App = () => {
+  const [searchField, setSearchField] = useState(''); // [value, setValue]
+  const [subTitle, setSubTitle] = useState('');
+  const [monsters, setMonsters] = useState<Monster[]>([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>('https://jsonplaceholder.typicode.com/users');
+      setMonsters(users);
+    }
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField (searchFieldString);
+  };
+
+  const onSubTitleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSubTitle (searchFieldString);
+  };
+
+  return (
+    <div className="App">
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>{subTitle}</h1>
+
+      <SearchBox 
+        className='monsters-search-box'
+        placeholder='Search Monsters' 
+        onChangeHandler={onSearchChange}  
+      />
+      <br />
+      <SearchBox 
+        className='subtitle-search-box'
+        placeholder='set subTitle' 
+        onChangeHandler={onSubTitleChange}  
+      />
+
+      <CardList monsters={filteredMonsters} />
+
+  </div>
+  )
+}
+
+export default App;
